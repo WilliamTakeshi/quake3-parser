4da16754db7b90817af48a5e8ef1f9f5ccae234f
William Takeshi <williamtpereira@gmail.com>
2024-02-18 17:46:02 -0300

    first version of the report

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use serde::Serialize;
 mod parser;
 mod event;
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 pub struct MatchKills<'a> {
     total_kills: u32,
     players: Vec<&'a str>,
@@ -14,6 +14,9 @@ pub struct MatchKills<'a> {
 fn main() -> Result<(), &'static str> {
     let args: Vec<String> = env::args().collect();
 
+    let mut match_struct = HashMap::new();
+    let mut match_num = 0;
+
     if args.len() < 2 {
         Err("Insufficient arguments")
     } else {
@@ -21,10 +24,46 @@ fn main() -> Result<(), &'static str> {
         let lines = file.lines();
         for line in lines {
             let log = parser::parse_line(line);
-            println!("{:?}", log);
+            let Ok((_, event)) = log else {
+                println!("Error parsing line: {}", line);
+                continue;
+            };
+            match event {
+                event::Event::Kill(kill) => {
+                    let match_number = format!("match_{}", match_num);
+                    match_struct.entry(match_number.clone()).or_insert(MatchKills {
+                        total_kills: 0,
+                        players: vec![],
+                        kills: HashMap::new(),
+                    });
+                    let match_kill = match_struct.get_mut(&match_number).unwrap();
+                    match_kill.total_kills += 1;
+                    if kill.killer == "<world>" {
+                        match_kill.kills.entry(kill.victim).or_insert(0);
+                        match_kill.kills.entry(kill.victim).and_modify(|e| *e -= 1);
+                        if !match_kill.players.contains(&kill.victim) {
+                            match_kill.players.push(kill.victim);
+                        }
+                    } else {
+                        match_kill.kills.entry(kill.killer).or_insert(0);
+                        match_kill.kills.entry(kill.killer).and_modify(|e| *e += 1);
+                        match_kill.kills.entry(kill.victim).or_insert(0);
+                        if !match_kill.players.contains(&kill.killer) {
+                            match_kill.players.push(kill.killer);
+                        }
+                        if !match_kill.players.contains(&kill.victim) {
+                            match_kill.players.push(kill.victim);
+                        }
+                    }
+                }
+                event::Event::InitGame => {
+                    match_num += 1;
+                }
+                event::Event::ShutdownGame => {}
+                _ => {}
+            }
         }
-        // let report = generate_report(&file);
-        // println!("{}", serde_json::to_string_pretty(&report).unwrap());
+        println!("{}", serde_json::to_string_pretty(&match_struct).unwrap());
         Ok(())
     }
 }
